e2f971e725c9001938fa71acabcbcff1b093ba1f
Ashish Gupta <haitomns4173@gmail.com>
2025-07-31 08:56:30 +0545

    Updated the jenkins again again

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ def submit():
     return render_template("result.html", name=name, email=email, message=message)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
